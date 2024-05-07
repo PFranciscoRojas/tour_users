3da73ee326a13fa373a8682510169112c2050f9f
pfranciscorojas <franciscorojas92@gmail.com>
2024-05-07 14:43:21 -0500

    change core

--- src/main/java/com/tour/user/security/SecurityConfig.java
+++ src/main/java/com/tour/user/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));  // Configura tus orígenes
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
